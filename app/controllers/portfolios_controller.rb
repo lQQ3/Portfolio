929e87e338151c736844c4dbd2c193f2f216042b
Shawn <Shawnrice2332@hotmail.com>
2019-07-20 18:39:55 -0700

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,77 +1,78 @@
 class PortfoliosController < ApplicationController
-	before_action :set_porfolio_item, only: [:edit, :update, :show, :destroy]
-	layout 'portfolio'
-	access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-
-	def index
-		@portfolio_items = Portfolio.by_position
-	end
-
-	def angular
-		@angular_portfolio_items = Portfolio.angular
-	end
-
-	def new
-		@portfolio_item = Portfolio.new
-		3.times { @portfolio_item.technologies.build}
-	end
-
-	def create
-	    @portfolio_item = Portfolio.new(portfolio_params)
-
-	    respond_to do |format|
-	      if @portfolio_item.save
-	        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-	        format.json { render :show, status: :created, location: @portfolio_item }
-	      else
-	        format.html { render :new }
-	        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-	      end
-	    end
-	end
-
-	def edit
-	end
-	
-	def update
-
-	    respond_to do |format|
-	      if @portfolio_item.update(portfolio_params)
-	      	# params.require(:portfolio).permit(:title, :subtitle, :body))
-	        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-	        format.json { render :show, status: :ok, location: @blog }
-	      else
-	        format.html { render :edit }
-	        format.json { render json: @blog.errors, status: :unprocessable_entity }
-	      end
-    	end
-  	end
-
-  	def show
-   	end
-
-	def destroy
-		# Destroy/delete the record
-		@portfolio_item.destroy
-
-		# Reedirect
-	    respond_to do |format|
-	      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
-	      format.json { head :no_content }
-	  	end
-  	end
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
-  	private
+  def index
+    @portfolio_items = Portfolio.by_position
+  end
 
-  	def portfolio_params
-  		params.require(:portfolio).permit(:title, 
-  										  :subtitle,
-  										  :body,
-  										  technologies_attributes: [:name]
-  										  )
-  	end
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
 
-  	def set_porfolio_item
-  		@portfolio_item = Portfolio.find(params[:id])
-  	end
+    render json: { data: @portfolio_item }
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
