eed9cfcfdbbab70af2630e9b21de1c7580db6e25
Shawn <Shawnrice2332@hotmail.com>
2019-07-24 17:23:28 -0700

    Updated file upload forms and refactored placeholde functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,13 +1,16 @@
 module PortfoliosHelper
+	def image_generator(height:, width:)
+		"https://via.placeholder.com/#{height}x#{width}"
+	end
 
-  def portfolio_img img, type
-    if !img.nil?
-      img
-    elsif type == 'thumb'
-      image_generator(height: '350', width: '200')
-    elsif 
-      image_generator(height: '600', width: '400')
-    end
-  end
+	def portfolio_img img, type
+		if img.model.main_image? || img.model.thumb_image?
+	    	img.to_s
+	    elsif type == 'thumb'
+	    	image_generator(height: '350', width: '200')
+	    elsif 
+	      	image_generator(height: '600', width: '400')
+	    end
+	end
 
 end
--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,13 +1,21 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :file
+  	include CarrierWave::MiniMagick
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  	storage :file
 
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  	def store_dir
+    	"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  	end
+  	def extension_whitelist
+    	%w(jpg jpeg gif png)
+  	end
 
+  	process resize_to_fit: [800, 800]
+
+  	version :thumb do
+
+    	process resize_to_fill: [200, 200]
+
+	end
 end
