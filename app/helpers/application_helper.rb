acd0377c3cca28f7901fe024313a33d190dbc847
Shawn <Shawnrice2332@hotmail.com>
2019-07-25 02:51:10 -0700

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,4 +65,16 @@ module ApplicationHelper
  		"active" if current_page? path
  	end
 
+
+ 	def alerts
+ 		alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+ 		if alert
+ 			alert_generator alert
+ 		end
+ 	end
+
+ 	def alert_generator msg
+ 		js add_gritter(msg, title: "Shawn Rice Portfolio", sticky: false)
+ 	end
  end
